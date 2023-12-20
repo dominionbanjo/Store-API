2bf7eb08b39bee137a92d547c9f7780d112085ad
dominionbanjo <DomIdee001#&>
2023-12-20 10:57:20 +0100

    First Commit

---
+++ app.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
+const app = express();
+const connectDB = require("./db/connect");
+
+const productsRouter = require("./routes/products");
+const morgan = require("morgan");
+const notFound = require("./middleware/not-found");
+const errorHandler = require("./middleware/error-handler");
+
+//Routes
+
+app.get("/", (req, res) => {
+  res.send("Store Api");
+});
+
+app.use(morgan("dev"));
+
+app.use("/api/v1/products", productsRouter);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ controllers/products.js
@@ -0,0 +1,19 @@
+const Product = require("../models/product");
+
+const getAllProductsStatic = async (req, res) => {
+  const products = await Product.find({ featured: true });
+  res.status(200).json({ products, nbHits: products.length });
+};
+
+const getAllProducts = async (req, res) => {
+  const { featured } = req.query;
+  queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+
+  const products = await Product.find(queryObject);
+  res.status(200).json({ products, nbHits: products.length });
+};
+
+module.exports = { getAllProducts, getAllProductsStatic };
